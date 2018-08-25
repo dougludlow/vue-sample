dd2ffff1d1e20f933c2e2137e6896dec846c62bf
Douglas Ludlow <doug.ludlow@royaljay.net>
2018-08-25 17:54:21 -0600

    Add demo.

---
+++ docs/precache-manifest.7a8ba3b0cca6ffc8e3c37810508a9c4d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/images/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "07cedcf9faf0e4fce6fe",
+    "url": "/css/app.5098177f.css"
+  },
+  {
+    "revision": "d04645c00227994fd4d0",
+    "url": "/css/chunk-vendors.3cd5f8f5.css"
+  },
+  {
+    "revision": "d04645c00227994fd4d0",
+    "url": "/js/chunk-vendors.cc94dd05.js"
+  },
+  {
+    "revision": "fa8606fd5e9356535dc5e8957d39f938",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/images/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/images/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/images/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/images/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/images/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "07cedcf9faf0e4fce6fe",
+    "url": "/js/app.45c5564c.js"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/images/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/images/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/images/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/images/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/images/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/images/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "f22d501a35a87d9f21701cb031f6ea17",
+    "url": "/images/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "b048812bfda4634f8a60f067cadf629a",
+    "url": "/rest_hours.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d469a56d71785ef37a5592b74bcfdaa9",
+    "url": "/assets/restaurant.mp4"
+  }
+];
